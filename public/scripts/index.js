02ecae7200af986dce62f4464049d7aaf24f23b4
randhayoga <92174528+randhayoga@users.noreply.github.com>
2024-03-08 23:06:08 +0700

    feat: filtering button now works

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,43 @@
 import cars from "./cars.js";
 
-const allCars = cars.getAllCars();
-console.log(allCars);
+// Element constants declaration
+const filteredCard = document.getElementById("carCard");
+const filterButton = document.getElementById("searchBtn");
 
-const filteredCars = cars.filterCars(2, "2022-04-23", "00:00:00");
-console.log(filteredCars);
+// Main
+filterButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // Form constant declaration
+  const desiredCap = document.getElementById("passengerCount").value;
+  const desiredDate = document.getElementById("dateInput").value;
+  const desiredTime = document.getElementById("timeInput").value;
+  const arrOfFilteredCars = cars.filterCars(
+    desiredCap,
+    desiredDate,
+    desiredTime
+  );
+
+  let filteredCardWithCars = "";
+
+  arrOfFilteredCars.map((filteredCar) => {
+    filteredCardWithCars += `<div class="col-lg-6 col-xl-4 mb-4">
+        <div class="card shadow-sm p-3 bg-white h-100">
+          <img src="${filteredCar.image}" class="img-fluid" style="max-height: 275px; object-fit: cover;">
+          <p class="card-text fw-medium" style="margin-top: 10px; margin-bottom: 5px;">${filteredCar.manufacture} ${filteredCar.model}</p>
+          <h5 class="card-title">Rp ${filteredCar.rentPerDay} / hari</h5>
+          <p class="card-text">${filteredCar.description}</p>
+          <span><img src="../node_modules/bootstrap-icons/icons/people.svg" style="height: 22px;">&ensp;${filteredCar.capacity} orang</span>
+          <span class="mt-2"><img src="../node_modules/bootstrap-icons/icons/gear.svg"
+              style="height: 22px;">&ensp;${filteredCar.transmission}</span>
+          <span class="mt-2"><img src="../node_modules/bootstrap-icons/icons/calendar4.svg"
+              style="height: 22px;">&ensp;Tahun ${filteredCar.year}</span>
+          <div class="card-body d-flex flex-column">
+            <button type="button" class="btn btn-primary mt-auto" style="color: #ffffff; width: 100%">Pilih Mobil</button>
+          </div>
+        </div>
+      </div>`;
+  });
+
+  filteredCard.innerHTML = filteredCardWithCars;
+});
